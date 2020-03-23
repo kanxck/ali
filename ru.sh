d174f0d7a2354c7f9675837e3f804e1ca3ecd9b4
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-23 23:43:25 +0700

    Delete ru.sh

--- ru.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install unzip
-wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar3/sugarmaker-v2.5.0-sugar3-linux64-static.zip
-unzip sugarmaker-v2.5.0-sugar3-linux64-static.zip
-cd sugarmaker-v2.5.0-sugar3-linux64-static
